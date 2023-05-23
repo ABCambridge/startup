7965e5814a5cc95e8f8cbfddde90ae60b71c9be0
Andrew Cambridge <andrewbcambridge@gmail.com>
2023-05-23 14:49:53 -0600

    js:logout - implemented logout functionality.

--- login.js
+++ login.js
@@ -1,3 +1,22 @@
+const AUTH_KEY = "authtoken";
+//TODO: remove this const
+const TEST_AUTH_TOKEN = "test_auth_token_1001";
+const TEST_USERNAME = "Andrew";
+const TEST_PASSWORD = "Cambridge";
+
+function verifyAuthForLogin(){
+    const authtoken = localStorage.getItem(AUTH_KEY);
+    if(authtoken !== null){
+        //VERIFY authtoken with server and get username if it is valid. Else, just go to index.html like normal
+        if(authtoken === TEST_AUTH_TOKEN){
+            localStorage.setItem("username",TEST_USERNAME);
+            window.location.href = "messageHome.html";
+        }
+    }
+}
+
+verifyAuthForLogin();
+
 function login(){
     const username = document.querySelector("#username").value;
     const password = document.querySelector("#password").value;
@@ -10,10 +29,9 @@ function validateCredentials(username,password){
     }
     else{
         //THIS IS WHERE YOU CALL THE SERVER AND DETERMINE VALID LOGINS  (call another function)
-        if(username === "Andrew" && password === "Cambridge"){
-            const authtoken = "test_auth_token_1001";
+        if(username === TEST_USERNAME && password === TEST_PASSWORD){
             localStorage.setItem("username",username);
-            localStorage.setItem("authtoken",authtoken);
+            localStorage.setItem(AUTH_KEY,TEST_AUTH_TOKEN);
             window.location.href = "messageHome.html";
         }
         else{
--- messageHome.js
+++ messageHome.js
@@ -1,3 +1,31 @@
+const AUTH_KEY = "authtoken";
+//TODO: remove this const
+const TEST_AUTH_TOKEN = "test_auth_token_1001";
+const TEST_USERNAME = "Andrew";
+const TEST_PASSWORD = "Cambridge";
+
 function verifyAuth(){
-    const authtoken = localStorage.getItem("authtoken");
+    const authtoken = localStorage.getItem(AUTH_KEY);
+    //TODO: verify authoken with the database. Reject if not found
+    if(authtoken !== TEST_AUTH_TOKEN){
+        window.location.href = "index.html";
+        alert("Session authentication failed.");
+    }
+    else{
+        
+    }
+}
+
+verifyAuth();
+
+function updateUserDisplay(){
+
+}
+
+updateUserDisplay();
+
+function logout(){
+    localStorage.removeItem(AUTH_KEY);
+    localStorage.removeItem("username");
+    window.location.href = "index.html";
 }
