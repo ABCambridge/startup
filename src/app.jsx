2ef9bd1b6472ed43a8a252574ab649a1371e643f
Andrew Cambridge <andrewbcambridge@gmail.com>
2023-06-15 09:43:01 -0600

    BrowserRouter and Routes are in place, but not used.

--- src/app.jsx
+++ src/app.jsx
@@ -5,6 +5,7 @@ import { CreateAccount } from './createAccount/createAccount.jsx';
 import { MessageHome } from './messageHome/messageHome.jsx';
 import { Settings } from './settings/settings.jsx';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 
 export default function App(){
     const [child, setChild] = React.useState("login");
@@ -12,16 +13,24 @@ export default function App(){
     let Next = getChild(child);
 
     return (
-        <div className='body'>
-            <header className="pageHeader borderItem">
-                <h1 className="menuButton pageTitle" >FastChat</h1>
-            </header>
-            <Next transitionScreen={setChild} authorized={authorized} setAuth={setAuth} />
-            <footer className="borderItem">
-                <a href="https://github.com/ABCambridge/startup.git">Andrew's GitHub for Startup</a>
-                <p>Authored by Andrew Cambridge</p>
-            </footer>
-        </div>
+        <BrowserRouter>
+            <div className='body'>
+                <header className="pageHeader borderItem">
+                    <h1 className="menuButton pageTitle" >FastChat</h1>
+                </header>
+                <Next transitionScreen={setChild} authorized={authorized} setAuth={setAuth} />
+                <footer className="borderItem">
+                    <a href="https://github.com/ABCambridge/startup.git">Andrew's GitHub for Startup</a>
+                    <p>Authored by Andrew Cambridge</p>
+                </footer>
+            </div>
+            <Routes>
+                <Route path='/' element={<Login transitionScreen={setChild} authorized={authorized} setAuth={setAuth}/>} exact />
+                <Route path='/messageHome' element={<MessageHome transitionScreen={setChild} authorized={authorized} setAuth={setAuth}/>} exact />
+                <Route path='/createAccount' element={<CreateAccount transitionScreen={setChild} authorized={authorized} setAuth={setAuth}/>} exact />
+                <Route path='/settings' element={<Settings transitionScreen={setChild} authorized={authorized} setAuth={setAuth}/>} exact />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
